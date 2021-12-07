18bfbf35657c5c77a04737f50994d4399e175fe5
David Callizaya <davidcallizaya@gmail.com>
2021-12-07 16:39:59 -0400

    Update slide.php

--- public/david/rooms/room2/slide.php
+++ public/david/rooms/room2/slide.php
@@ -1,10 +1,10 @@
 <h2 class="slide__name"></h2>
 <h3 class="slide__title">
-    Día mundial de la filosofía
+    Voluntario en Nueva Acrópolis
     <div class="slide__number">
         <a href="https://www.facebook.com/NuevaAcropolisLaPaz" target="_blank">
             Nueva Acropolis
         </a>
     </div>
 </h3>
-<p class="slide__date">tercer jueves de noviembre de cada año</p>
+<p class="slide__date"></p>
